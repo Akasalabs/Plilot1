5a966fa0431e05209a9a6a783332189cf1e4cab1
RaghuVamz <raghuvamsi1992@gmail.com>
2017-05-16 15:32:28 +0530

    reverting old code

--- chaincode/asset_1.go
+++ chaincode/asset_1.go
@@ -226,7 +226,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 		return nil, errors.New(jsonResp)
 	}
 	fmt.Println("read contract output ", valAsbytes)
-	return []byte("randomvalue"), nil
+	return valAsbytes, nil
 }
 
 func (t *SimpleChaincode) getAllKeys(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
